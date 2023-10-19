27a6b8d038b65720fbbec6bd15b022e56d139263
Iago Sousa <sousair2001@gmail.com>
2023-10-19 16:12:14 -0300

    feat: open and read lines form .vm file

--- vm-translator.go
+++ vm-translator.go
@@ -7,11 +7,34 @@ import (
 
 func main() {
 	check_args()
+
+	filename := os.Args[1]
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		panic("Cannot open VM file")
+	}
+	defer file.Close()
+
+	lineScanner := bufio.NewScanner(file)
+	for lineScanner.Scan() {
+		line := strings.TrimSpace(lineScanner.Text())
+
+		println(line)
+
+		if line == "" || strings.HasPrefix(line, "//") {
+			continue
+		}
+	}
+
+	if err := lineScanner.Err(); err != nil {
+		panic("Error while reading VM file")
+	}
 }
 
-func check_args() (valid bool) {
+func check_args() {
 	args := os.Args
-
 	if len(args) != 2 {
 		panic("Usage: vm-translator <File.vm>")
 	}
